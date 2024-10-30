6d0b3d4b2b51a53750f58b63142628c2c16779a9
David Stinnett <davidstinnett@icloud.com>
2024-10-29 21:00:39 -0400

    added new links to header bar

--- emma/src/components/Header/Header.js
+++ emma/src/components/Header/Header.js
@@ -1,7 +1,10 @@
+// src/components/Header.js
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+
+import "./Header.css"
 
 function Header() {
   return (
@@ -9,18 +12,23 @@ function Header() {
       <Navbar.Brand as={Link} to="/">EMMA</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
+        {/* Left-aligned navigation links */}
+        <Nav className="mr-auto">
           <Nav.Link as={HashLink} smooth to="/#about">
             About
           </Nav.Link>
           <Nav.Link as={HashLink} smooth to="/#contact">
             Get in Touch
           </Nav.Link>
-          <Nav.Link as={Link} to="/apply">
-           Interested?
+          <Nav.Link as={Link} to="/meet-the-team">
+            Meet the Team
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
+      {/* Right-aligned Employee Login Button outside Navbar.Collapse */}
+      <Button className="login" as={Link} to="/employee-login" variant="outline-light" className="ml-auto">
+        Employee Login
+      </Button>
     </Navbar>
   );
 }
