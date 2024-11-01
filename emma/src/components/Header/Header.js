644b1ec86c13e5f2fd708d35185706d72420668c
David Stinnett <davidstinnett@icloud.com>
2024-11-01 18:53:20 -0400

    tweaked mobile styling

--- emma/src/components/Header/Header.js
+++ emma/src/components/Header/Header.js
@@ -3,20 +3,22 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
 import './Header.css'
 
 function Header() {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 991);
 
-  // Update `isMobile` state on window resize
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth <= 991);
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  // Function to close the navbar when a link is clicked
   const handleLinkClick = () => {
     if (isCollapsed) {
       setIsCollapsed(false);
@@ -33,38 +35,31 @@ function Header() {
       className="custom-navbar"
     >
       <Navbar.Brand as={Link} to="/" className="brand-padding">EMMA</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setIsCollapsed(!isCollapsed)} />
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className={`navbar-toggler ${isCollapsed ? "toggler-active" : ""}`}
+      >
+        <span className="custom-toggler-icon">
+          <FontAwesomeIcon icon={faBars} color="var(--electric-blue)" />
+        </span>
+      </Navbar.Toggle>
       <Navbar.Collapse id="basic-navbar-nav" className="navbar-collapse-custom">
-        {/* Left-aligned navigation links */}
         <Nav className="nav-links" onClick={handleLinkClick}>
-          <Nav.Link as={HashLink} smooth to="/#about">
-            About
-          </Nav.Link>
-          <Nav.Link as={HashLink} smooth to="/#contact">
-            Get in Touch
-          </Nav.Link>
-          <Nav.Link as={Link} to="/meet-the-team">
-            Meet the Team
-          </Nav.Link>
-          <Nav.Link as={Link} to="/meet-the-team">
-            What is an apprenticeship?
-          </Nav.Link>
+          <Nav.Link as={HashLink} smooth to="/#about">About</Nav.Link>
+          <Nav.Link as={HashLink} smooth to="/#contact">Get in Touch</Nav.Link>
+          <Nav.Link as={Link} to="/meet-the-team">Meet the Team</Nav.Link>
+          <Nav.Link as={Link} to="/apprenticeship-info">What is an apprenticeship?</Nav.Link>
         </Nav>
-
-        {/* Conditionally render Employee Login Button based on screen size */}
         {isMobile ? (
           <Nav className="employee-login-sm">
             <Nav.Item className="mt-3">
-              <Button as={Link} to="/employee-login" variant="outline-light" block>
-                Employee Login
-              </Button>
+              <Button as={Link} to="/employee-login" variant="outline-light" block>Employee Login</Button>
             </Nav.Item>
           </Nav>
         ) : (
           <div className="employee-login-lg">
-            <Button as={Link} to="/employee-login" variant="outline-light">
-              Employee Login
-            </Button>
+            <Button as={Link} to="/employee-login" variant="outline-light">Employee Login</Button>
           </div>
         )}
       </Navbar.Collapse>
